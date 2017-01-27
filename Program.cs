41081d5c99910f5eff3521f17137fcc94b75ef7b
Matt Wind <mattwind@gmail.com>
2017-01-26 23:47:12 -0800

    Added args

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        if (args == null)
+        {
+            Console.WriteLine("args is null");      // Check for null array
+        }
+        else
+        {
+            Console.Write("args length is ");
+            Console.WriteLine(args.Length);         // Write array length
+            for (int i = 0; i < args.Length; i++)   // Loop through array
+            {
+                string argument = args[i];
+                Console.Write("args index ");
+                Console.Write(i);                   // Write index
+                Console.Write(" is [");
+                Console.Write(argument);            // Write string
+                Console.WriteLine("]");
+            }
+        }
+        Console.ReadLine();
         }
     }
 }
